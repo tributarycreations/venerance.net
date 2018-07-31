c5f169fe5b09fe57758e3a32cab16dadae72144d
James McLean <jmcleanbos@gmail.com>
2018-07-31 16:12:34 -0400

    Added bootstrap base css and js

--- dist/script.c0059de7.js
+++ dist/script.c0059de7.js
@@ -202,7 +202,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = '' || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + '51723' + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + '52096' + '/');
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
 
