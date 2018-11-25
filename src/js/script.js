6cffc6ec142dec192505c1c7517d8eda0edc60a9
James McLean <jmcleanbos@gmail.com>
2018-11-25 18:24:50 -0500

    Reworked navigation, fixing the border-bottom from adding space and making the nav scroll directly to the elements

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,27 @@
-import '../scss/style.scss';
+import "../scss/style.scss";
+
+$(document).ready(function() {
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 200) {
+      $(".navbar").css("background-color", "rgba(0,0,0,0.65)");
+      $(".navbar-brand").css("display", "block");
+    } else {
+      $(".navbar").css("background-color", "transparent");
+      $(".navbar-brand").css("display", "none");
+    }
+  });
+});
+
+$(".navbar ul li a").on("click", function(e) {
+  e.preventDefault();
+  var hash = this.hash;
+  $("html, body").animate(
+    {
+      scrollTop: $(hash).offset().top
+    },
+    1000,
+    function() {
+      window.location.hash = hash;
+    }
+  );
+});
