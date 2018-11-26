e130bccfeeaef195068c606504b30b68a057742b
James McLean <jmcleanbos@gmail.com>
2018-11-25 23:35:58 -0500

    Added S2 Creation Kit embed .. to be later refactired into a modal

--- script.98db9c26.js
+++ script.98db9c26.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,n,t){var i="function"==typeof parcelRequire&&parcelRe
 },{"./../img/nanosuitmjolnir.jpg":[["nanosuitmjolnir.c8ed11de.jpg","8D/t"],"8D/t"],"./../img/junglestealthgpost_ani.png":[["junglestealthgpost_ani.1339ce9c.png","gX/x"],"gX/x"],"./../img/view2.jpg":[["view2.857fc740.jpg","cJAl"],"cJAl"],"./../img/jockeyscavengers.png":[["jockeyscavengers.65f06931.png","KF7J"],"KF7J"]}],"L4bL":[function(require,module,exports) {
 "use strict";require("../scss/style.scss"),$(document).ready(function(){$(window).scroll(function(){$(this).scrollTop()>200?($(".navbar").css("background-color","rgba(0,0,0,0.65)"),$(".navbar-brand").css("display","block")):($(".navbar").css("background-color","transparent"),$(".navbar-brand").css("display","none"))})}),$(".navbar ul li a").on("click",function(a){a.preventDefault();var n=this.hash;$("html, body").animate({scrollTop:$(n).offset().top},1e3,function(){window.location.hash=n})});
 },{"../scss/style.scss":"Gg4U"}]},{},["L4bL"], null)
-//# sourceMappingURL=script.01991a04.map
+//# sourceMappingURL=script.98db9c26.map
