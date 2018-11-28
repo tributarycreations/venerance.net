fe38d18ddf027efc049c4ddbbcedd9917a7a21f5
James McLean <jmcleanbos@gmail.com>
2018-11-27 21:56:06 -0500

    Added a slight drop shadow to navbar to differentiate it from the main content area

--- src/js/script.js
+++ src/js/script.js
@@ -3,9 +3,11 @@ import "../scss/style.scss";
 $(document).ready(function() {
   $(window).scroll(function() {
     if ($(this).scrollTop() > 75) {
-      $(".navbar").css("background-color", "rgba(48,58,43,0.90)");
+      $(".navbar").css("background-color", "rgba(48,58,43,0.85)");
+      $(".navbar").css("box-shadow", "1px 1px 1px rgba(48,58,43,1");
     } else {
       $(".navbar").css("background-color", "transparent");
+      $(".navbar").css("box-shadow", "2px 2px 2px transparent");
     }
   });
 });
